a5950d3b327b238360a00482471167d9f024e607
mxbossard <mxbossard@gmail.com>
2013-05-15 18:40:47 +0200

    Add fr.mby.utils.spring-utils to portal-core.

--- osgi-portal-core/src/main/java/fr/mby/portal/coreimpl/aggregator/AbstractReplyAggregator.java
+++ osgi-portal-core/src/main/java/fr/mby/portal/coreimpl/aggregator/AbstractReplyAggregator.java
@@ -18,13 +18,10 @@ package fr.mby.portal.coreimpl.aggregator;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.aop.support.AopUtils;
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationListener;
-import org.springframework.core.GenericTypeResolver;
 
 import fr.mby.portal.core.aggregator.IReplyAggregator;
 import fr.mby.portal.message.IReply;
+import fr.mby.utils.spring.aop.support.AopHelper;
 
 /**
  * @author Maxime Bossard - 2013
@@ -40,19 +37,11 @@ public abstract class AbstractReplyAggregator<T extends IReply> implements IRepl
 
 	@Override
 	public boolean supportsReplyType(final Class<? extends IReply> replyType) {
-		Class<?> typeArg = GenericTypeResolver.resolveTypeArgument(this.getClass(), IReplyAggregator.class);
-		if (typeArg == null || typeArg.equals(ApplicationEvent.class)) {
-			final Class<?> targetClass = AopUtils.getTargetClass(this);
-			if (targetClass != this.getClass()) {
-				typeArg = GenericTypeResolver.resolveTypeArgument(targetClass, ApplicationListener.class);
-			}
-		}
 
-		final boolean test = typeArg == null || typeArg.isAssignableFrom(replyType);
+		final boolean test = AopHelper.supportsType(this, replyType, IReplyAggregator.class);
 
-		AbstractReplyAggregator.LOG.debug(
-				"IReplyAggregator: [{}] supported type found: [{}] will supports [{}] type ? => [{}].",
-				this.getClass(), typeArg, replyType, test);
+		AbstractReplyAggregator.LOG.debug("[{}] will supports [{}] type ? => [{}].", this.getClass().getSimpleName(),
+				replyType.getSimpleName(), test);
 
 		return test;
 	}
