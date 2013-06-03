847d7fc456dd771f3701e55b6f11be179569ad04
mxbossard <mxbossard@gmail.com>
2013-06-03 08:25:42 +0200

    Test correction

--- osgi-portal-js/test/unit/mbyUtils/ToolsTest.js
+++ osgi-portal-js/test/unit/mbyUtils/ToolsTest.js
@@ -5,19 +5,24 @@ describe("MbyUtils.Tools unit test.", function() {
 
 		expect(array.length).toBe(5);
 
+		console.log(array);
+
 		var removed = array.remove(5);
+		console.log(array);
 		expect(removed).toBe(true);
 		expect(array.length).toBe(4);
-		expect(array[0]).toBe(3);
-		expect(array[2]).toBe(9);
+		expect(array[0]).toBe(2);
+		expect(array[2]).toBe(7);
 
 		removed = array.remove(2);
+		console.log(array);
 		expect(removed).toBe(true);
 		expect(array.length).toBe(3);
 		expect(array[0]).toBe(3);
 		expect(array[2]).toBe(9);
 
 		removed = array.remove(9);
+		console.log(array);
 		expect(removed).toBe(true);
 		expect(array.length).toBe(2);
 		expect(array[0]).toBe(3);
