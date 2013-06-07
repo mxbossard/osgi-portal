5d26d61ec860140accaeac3d31698699b7b56ac6
mxbossard <maxime.bossard@recia.fr>
2013-06-07 08:23:38 +0200

    Add the possibility to listen event whith Regexp topic in EventTarget.

--- osgi-portal-js/test/unit/mbyUtils/event/EventTargetTest.js
+++ osgi-portal-js/test/unit/mbyUtils/event/EventTargetTest.js
@@ -59,6 +59,54 @@ describe("MbyUtils.event.EventTarget unit test.", function() {
 		expect(testValue).toBe(4);
 	});
 
+	it("fireEvent() test with regexp listener", function() {
+		var target = new MbyUtils.event.EventTarget('testEventTarget');
+
+		// Fire an event will increment testValue.
+		var testValue = 0;
+		var testCallback = function() {
+			testValue++;
+		};
+		
+		var testCallback2 = function() {
+			testValue = testValue + 10;
+		};
+		
+		var testCallback3 = function() {
+			testValue = testValue + 100;
+		};
+		
+		var testCallback4 = function() {
+			testValue = testValue + 1000;
+		};
+		
+		var testCallback5 = function() {
+			testValue = testValue + 10000;
+		};
+		
+		target.addEventListener(new MbyUtils.event.EventListener('incrementValue', testCallback));
+		target.addEventListener(new MbyUtils.event.EventListener('.*Value', testCallback2));
+		target.addEventListener(new MbyUtils.event.EventListener('.*value', testCallback3));
+		target.addEventListener(new MbyUtils.event.EventListener('noTopic', testCallback4));
+		target.addEventListener(new MbyUtils.event.EventListener('.*', testCallback5));
+		
+		expect(testValue).toBe(0);
+
+		target.fireEvent(new MbyUtils.event.Event('incrementValue'));
+
+		expect(testValue).toBe(10011);
+
+		target.fireEvent(new MbyUtils.event.Event('otherTopic'));
+
+		expect(testValue).toBe(20011);
+
+		target.fireEvent(new MbyUtils.event.Event('incrementValue'));
+		target.fireEvent(new MbyUtils.event.Event('incrementValue'));
+		target.fireEvent(new MbyUtils.event.Event('incrementValue'));
+
+		expect(testValue).toBe(50044);
+	});
+	
 	it("removeEventListener() test", function() {
 		var target = new MbyUtils.event.EventTarget('testEventTarget');
 
